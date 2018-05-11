430195f8d4ffa84c82dda63b72ed714e1b17b936
maxig <maxig8@hotmail.com>
2018-05-11 17:47:16 -0300

    Tipos de Intents

--- app/src/main/java/example/com/prueba/MainActivity.java
+++ app/src/main/java/example/com/prueba/MainActivity.java
@@ -13,12 +13,36 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button cmd1 =  findViewById(R.id.button) ;
+        Button cmd1 = (Button) findViewById(R.id.cmd1) ;
         cmd1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent ( this, Main2Activity.class);
+                Intent intent =new Intent(view.getContext(), Main2Activity.class);
                 startActivity(intent);
+            }
+        });
+        Button cmd2 = (Button) findViewById(R.id.cmd2) ;
+        cmd2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent =new Intent(Intent.ACTION_SEND);
+                intent.setType("text/plain");
+                intent.putExtra(Intent.EXTRA_EMAIL, new String[]{"mail@mail.com"});
+                intent.putExtra(Intent.EXTRA_SUBJECT, "Este es un mail de prueba");
+                intent.putExtra(Intent.EXTRA_TEXT, "Mensaje texto");
+                Intent mailer = Intent.createChooser(intent, "Enviar mail usando");
+                startActivity(intent);
+            }
+        });
+
+        Button cmd3 = (Button) findViewById(R.id.cmd3) ;
+        cmd3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent =new Intent();
+                intent.setType("image/*");
+               intent.setAction( intent.ACTION_GET_CONTENT);
             }
         });
     }
