780e7795dd4e7bc6d4cb01c58eced1306663fa7d
maxig <maxig8@hotmail.com>
2018-05-11 17:26:49 -0300

    Tipos de Intents

--- app/src/main/java/example/com/prueba/MainActivity.java
+++ app/src/main/java/example/com/prueba/MainActivity.java
@@ -1,7 +1,10 @@
 package example.com.prueba;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button cmd1 =  findViewById(R.id.button) ;
+        cmd1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent ( this, Main2Activity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
